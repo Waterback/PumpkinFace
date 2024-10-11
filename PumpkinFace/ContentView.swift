6db66f3b4c9a42d65c9c35767b2b2990d9ed1a16
Martin Huber <martin.huber@innoq.com>
2024-10-11 10:35:31 +0200

    Initial Commit

---
+++ PumpkinFace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PumpkinFace
+//
+//  Created by Waterback on 11.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PumpkinFace/PumpkinFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpkinFaceApp.swift
+//  PumpkinFace
+//
+//  Created by Waterback on 11.10.24.
+//
+
+import SwiftUI
+
+@main
+struct PumpkinFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
